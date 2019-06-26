3efd51a411c48f0059779ebffdfb61babf9d234d
vpisaruc <vpisaruc@gmail.com>
2019-06-26 18:47:23 +0300

    Tables and XML

--- scraping.py
+++ scraping.py
@@ -1,9 +1,11 @@
 import bs4 as bs
 import urllib.request
+import pandas as pd
 
 sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
+"""
 # играемся с элементами navigation bar
 nav = soup.nav
 
@@ -18,4 +20,28 @@ for paragraph in body.find_all('p'):
 
 for div in soup.find_all('div', class_='body'):
     print(div.text)
+"""
 
+
+#table = soup.table
+table = soup.find('table')
+
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
+
+# поулчить таблицы с сайта при помощи pandas
+dfs = pd.read_html('https://pythonprogramming.net/parsememcparseface/', header=0)
+for df in dfs:
+    print(df)
+
+
+# работа с xml
+sauce = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()
+soup = bs.BeautifulSoup(sauce, 'xml')
+
+for url in soup.find_all('loc'):
+    print(url.text)
