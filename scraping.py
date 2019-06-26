bf57282f006d4401b5879a002808165459c157e3
vpisaruc <vpisaruc@gmail.com>
2019-06-26 18:35:03 +0300

    Navigation tags

--- scraping.py
+++ scraping.py
@@ -2,18 +2,20 @@ import bs4 as bs
 import urllib.request
 
 sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
-
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
-# поиск всех тегов p
-#print(soup.find_all('p'))
+# играемся с элементами navigation bar
+nav = soup.nav
 
-# поиск дочерних элементов в родительском
-# for paragraph in soup.find_all('p'):
-#      print(paragraph.text)
 
-# выдает весь текст на странице
-# print(soup.get_text())
-
-for url in soup.find_all('a'):
+for url in nav.find_all('a'):
     print(url.get('href'))
+
+# играемся с элементами body
+body = soup.body
+for paragraph in body.find_all('p'):
+    print(paragraph.text)
+
+for div in soup.find_all('div', class_='body'):
+    print(div.text)
+
